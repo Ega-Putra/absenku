31cba814a93c3e38949a3aba384e17e87ce347d7
Ega Putra <egaputraf@gmail.com>
2025-05-19 19:51:03 +0700

    update preview

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 website absensi sebagai bahan percobaan belajar node js mongodb dan chart js (still on  progress)
+![preview1](preview1.png)
+![preview2](preview2.png)
+![preview3](preview3.png)
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 const __dirname = path.resolve()
 const app = express()
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'frontend')))
@@ -68,7 +68,7 @@ app.get("/data", async (req, res) => {
     }
 })
 
-app.listen(PORT,() => {
+app.listen(port,() => {
     connectDB()
     console.log(`server running at http://localhost:${port}`)
 })
